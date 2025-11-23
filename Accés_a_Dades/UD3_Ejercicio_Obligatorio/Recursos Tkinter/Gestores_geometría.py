e20252963048eb8cb999f7d70c4aa5f10e677409
VB-gitH <vicberter@alu.edu.gva.es>
2025-11-23 13:20:56 +0100

    Eliminada carpeta UD3_Ejercicio_Obligatorio de Accés_a_Dades

--- Accés_a_Dades/UD3_Ejercicio_Obligatorio/Recursos Tkinter/Gestores_geometría.py
+++
@@ -1,8 +0,0 @@
-# Los gestores de geometría nos permiten establecer la posición relativa de los widgets 
-# dentro de los marcos o la ventana principal. Existen tres tipos:
-# El gestor pack() que se encarga de distribuir los widgets de un contenedor de forma
-# automática según los parámetros de configuración. 
-# El gestor grid() que se basa en el uso de una rejilla con filas y columnas para que
-# el/la programador/a pueda decidir dónde posicionar los widgets. 
-# El gestor place() que permite configurar las coordenadas (x, y) de cada widget para 
-# establecer su posición dentro del contenedor.
